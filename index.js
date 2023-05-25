e1db95d88756b0d7a298b296fca8df3422c56870
jabir <jaber@aybiz.com>
2023-05-25 13:55:37 +0300

    comm

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+const port = 9000;
+app.listen(port, () => {
+    console.log('Server Running', port);
+})
